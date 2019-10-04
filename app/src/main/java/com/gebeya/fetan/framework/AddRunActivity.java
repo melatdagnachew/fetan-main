76c88a031063872c97a59f4b39bd2c3d80dc5892
melatdagnachew <mel9dagnu@gmail.com>
2019-10-04 10:33:04 +0300

    Shared preference and room database

--- app/src/main/java/com/gebeya/fetan/framework/AddRunActivity.java
+++ app/src/main/java/com/gebeya/fetan/framework/AddRunActivity.java
@@ -4,9 +4,12 @@ import android.os.Bundle;
 import android.widget.EditText;
 
 import androidx.annotation.Nullable;
+import androidx.room.Room;
 
 import com.gebeya.fetan.R;
+import com.gebeya.fetan.data.AppDatabse;
 import com.gebeya.fetan.framework.base.BaseActivity;
+import com.gebeya.fetan.framework.util.Const;
 
 public class AddRunActivity extends BaseActivity {
 
@@ -14,9 +17,13 @@ public class AddRunActivity extends BaseActivity {
 
     EditText timeEditText;
 
+    private AppDatabse db;
+
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_run);
+
+        db= Room.databaseBuilder(this,AppDatabse.class, Const.DATABASE_NAME).build();
     }
 }
