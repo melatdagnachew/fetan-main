0205257bf994ffd13204656cf17fb4cf063bacb5
melatdagnachew <mel9dagnu@gmail.com>
2019-10-07 12:41:38 +0300

    Added Animation

--- app/src/main/java/com/gebeya/fetan/AboutActivity.java
+++ app/src/main/java/com/gebeya/fetan/AboutActivity.java
@@ -1,16 +1,45 @@
 package com.gebeya.fetan;
 
+import android.animation.AnimatorInflater;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import androidx.annotation.Nullable;
 
 import com.gebeya.fetan.framework.base.BaseActivity;
 
+import java.util.Random;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class AboutActivity extends BaseActivity {
+
+    @BindView(R.id.aboutLogoIon)
+    ImageView shoeLogo;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+        ButterKnife.bind(this);
+        animation();
     }
+    public void animation(){
+
+        AnimatorSet fabAnimation = (AnimatorSet)
+                AnimatorInflater.loadAnimator(this,R.animator.fab_animation);
+
+        fabAnimation.setTarget(shoeLogo);
+        fabAnimation.start();
+
+        ObjectAnimator animator = ObjectAnimator.ofFloat(shoeLogo,"rotation",0,-90);
+        animator.setDuration(1500);
+        animator.setStartDelay(400);
+        animator.start();
+
+    }
+
 }
--- app/src/main/java/com/gebeya/fetan/HomeActivity.java
+++ app/src/main/java/com/gebeya/fetan/HomeActivity.java
@@ -1,5 +1,8 @@
 package com.gebeya.fetan;
 
+import android.animation.AnimatorInflater;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
@@ -34,6 +37,18 @@ public class HomeActivity extends BaseActivity {
             final Intent intent = new Intent(getApplicationContext(), AddRunActivity.class);
             startActivity(intent);
         });
+
+        AnimatorSet fabAnimation = (AnimatorSet)
+                AnimatorInflater.loadAnimator(this,R.animator.fab_animation);
+
+        fabAnimation.setTarget(fab);
+        fabAnimation.start();
+
+        ObjectAnimator animator = ObjectAnimator.ofFloat(noRunsLabel,"alpha",0f,1f);
+        animator.setDuration(1000);
+        animator.setStartDelay(400);
+        animator.start();
+
     }
 
     @Override
